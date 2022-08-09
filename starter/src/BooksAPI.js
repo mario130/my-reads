449692f42210ad63db76315203180cffbeeb2d67
Mario Rizk <mario.rizk@softwaregroup.com>
2022-08-09 19:38:20 +0200

    feat: project done

--- starter/src/BooksAPI.js
+++ starter/src/BooksAPI.js
@@ -3,6 +3,9 @@ const api = "https://reactnd-books-api.udacity.com";
 let token = localStorage.token;
 
 if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
+else {
+    token = localStorage.token;
+}
 
 const headers = {
   Accept: "application/json",
